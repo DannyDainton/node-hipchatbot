41f2c2a781a14eb34a95f1d277fd65c9553e49bb
Danny Dainton <dannydainton@gmail.com>
2016-11-07 14:26:26 +0000

    Hipchat bot app files

---
+++ app.js
@@ -0,0 +1,19 @@
+// Dependencies
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); 
+
+// Route that Hipchat enters
+app.post("/hipchatbot", function(req, res) {
+
+  // Gets the message value from the Hipchat JSON webhook
+  var message = req.body.item.message.message;
+  
+  // Message posted back to Hipchat
+  res.json({ message: "You entered the following text: "+ message });
+
+});
+
+var port = Number(process.env.PORT || 3000);
+var server = app.listen(port);
