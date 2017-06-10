6f379a4cf3c094f5df2b2b3aeeb073e4da686651
Danny Dainton <dannydainton@gmail.com>
2017-06-10 23:06:54 +0100

    Update to ES6/ES7

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
 // Dependencies
-var bodyParser = require('body-parser');
-var express = require('express');
+const bodyParser = require('body-parser')
+const express    = require('express')
 
 // Calling the Express module
-var app = express();
+const app = express()
 
 // Using the JSON function of the body-parser module
-app.use(bodyParser.json()); 
+app.use(bodyParser.json()) 
 
 // The app can use the public folder to serve static files
-app.use(express.static('public'));
+app.use(express.static('public'))
 
 // Route that Hipchat enters
-app.post("/hipchatbot", function(req, res) {
+app.post("/hipchatbot", (req, res) => {
 
   // Gets the message value from the Hipchat JSON webhook
-  var message = req.body.item.message.message;
+  const message = req.body.item.message.message
   
   // Adding some low level logging for user requests
-  console.log("USER REQUEST: " + message);
+  console.log(`USER REQUEST: ${message}`)
   
   // Message posted back to Hipchat
-  res.json({ message: "You entered the following text: "+ message });
+  res.json({ message: `You entered the following text: ${message}` })
 
 });
 
 // Giving the app a port number to listen on - use 3000 by default 
-var port = Number(process.env.PORT || 3000);
+const port = Number(process.env.PORT || 3000)
 
 // Starts the app
-app.listen(port);
+app.listen(port)
