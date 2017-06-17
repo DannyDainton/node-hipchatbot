b03fdfe0420e7dcc9145db458538ab3798ec4c0c
Danny Dainton <dannydainton@gmail.com>
2017-06-17 10:26:50 +0100

    Added logging module and refactored

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // Dependencies
 const bodyParser = require('body-parser')
 const express    = require('express')
+const winston    = require('winston')
 
 // Calling the Express module
 const app = express()
@@ -17,13 +18,13 @@ app.post("/hipchatbot", (req, res) => {
   // Gets the message value from the Hipchat JSON webhook
   const message = req.body.item.message.message
   
-  // Adding some low level logging for user requests
-  console.log(`USER REQUEST: ${message}`)
+  // Adding some low level logging so that you can see the requests in the console
+  winston.info(`User Request: ${message}`)
   
   // Message posted back to Hipchat
   res.json({ message: `You entered the following text: ${message}` })
 
-});
+})
 
 // Giving the app a port number to listen on - use 3000 by default 
 const port = Number(process.env.PORT || 3000)
