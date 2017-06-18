6011fcfa446ea2090643bdc76de2f2cb551f3404
Danny Dainton <dannydainton@gmail.com>
2017-06-18 08:42:24 +0100

    Removed the comments

--- app.js
+++ app.js
@@ -1,42 +1,25 @@
-// Dependencies
 const bodyParser = require('body-parser')
 const express    = require('express')
 const winston    = require('winston')
 
-// Calling the Express module
 const app = express()
-
-// Using the JSON function of the body-parser module
 app.use(bodyParser.json()) 
-
-// The app can use the public folder to serve static files
 app.use(express.static('public'))
 
-// Route that Hipchat enters
 app.post("/hipchatbot", (req, res) => {
-
-  // Gets the message value from the Hipchat JSON webhook
   const message     = req.body.item.message.message
   const name        = req.body.item.message.from.name
   
-  // Adding some low level logging so that you can see the requests in the console
   winston.info(`Message Sent: ${message}`)
   winston.info(`Sent By: ${name}`)
   
-  // Message posted back to Hipchat
   res.json({ message: `${name} entered the following text: ${message}` })
-
 })
 
-// Giving the app a port number to listen on - use 3000 by default 
 const port = Number(process.env.PORT || 7000)
-
-// Starts the app
 if(!module.parent) { 
   app.listen(port)
 }
-// Log that the server has started
 winston.info(`Server Started on port: ${port}`)
 
-// Exporting the app so that it can be used in the tests
 module.exports = app
